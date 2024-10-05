c740b28b9cbc207cd9d2e30464a9db22171bb490
whereissam <sam135642@gmail.com>
2024-10-05 23:03:02 +0800

    feat: add mongoose

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 5.0,
+  price: 497
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR: ', err);
+  });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port} ...`)
-})
+  console.log(`App running on port ${port}...`);
+});
